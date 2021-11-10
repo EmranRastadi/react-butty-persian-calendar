4bf38ab18dd99557947d7049cd0ddbf86bed37dd
Emran Rastadi Poorak <emran.rastadi@gmail.com>
2021-11-11 00:50:21 +0330

    del

--- src/Calendar/types/index.ts
+++
@@ -1,9 +0,0 @@
-export type eventsType = {
-    date: {
-        year: string,
-        month: string,
-        monthNum: string,
-        day: string
-    },
-    timestamp: number
-}
--- src/index.tsx
+++
@@ -1,1 +0,0 @@
-export {default as ButtyCalendar} from './Calendar'
